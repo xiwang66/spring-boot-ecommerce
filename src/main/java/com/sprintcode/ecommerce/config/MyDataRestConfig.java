fdbc9e33f3de5a0a7e77c6b85849793cc2c0134b
Xi Wang <xiwang6688@gmail.com>
2023-02-07 14:47:25 -0500

    add support for order to allow logged-in users to retrieve order history

--- src/main/java/com/sprintcode/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/sprintcode/ecommerce/config/MyDataRestConfig.java
@@ -1,9 +1,6 @@
 package com.sprintcode.ecommerce.config;
 
-import com.sprintcode.ecommerce.entity.Country;
-import com.sprintcode.ecommerce.entity.Product;
-import com.sprintcode.ecommerce.entity.ProductCategory;
-import com.sprintcode.ecommerce.entity.State;
+import com.sprintcode.ecommerce.entity.*;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +39,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
         disableHttpMethods(Country.class, config, theUnsupportedActions);
         disableHttpMethods(State.class, config, theUnsupportedActions);
+        disableHttpMethods(Order.class, config, theUnsupportedActions);
 
         // call an internal helper method
         exposeIds(config);
